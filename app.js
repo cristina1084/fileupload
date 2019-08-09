d2c1eb8f5b7e0dbd683eb9638e26b27340e9b8fa
cristina1084 <cristina.johns1084@gmail.com>
2019-08-09 21:31:04 +0530

    third commit

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
-const express = require('express');
-//const path = require('path');
+const express = require("express");  
+const multer = require('multer');  
 
-const app = express();
+const app = express();  
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("view engine","ejs");  
+app.set("views","views");  
 
+var storage =   multer.diskStorage({  
+  destination: (req, file, callback)=>{  
+    callback(null, './public/images');  
+  },  
+  filename: (req, file, callback)=>{  
+    callback(null, file.originalname);  
+  }  
+});  
 
-//app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.listen(8080,()=>{
-  console.log("Listening");
-})
-
+var upload = multer({ storage : storage}).single('myfile');  
+  
+app.get('/',(req,res)=>{  
+      res.render("file");  
+});  
+  
+app.post('/upload',(req,res)=>{  
+    upload(req,res,(err)=>{  
+        if(err) return res.end("Error uploading file.");  
+        else res.end("File is uploaded successfully!");  
+    });  
+});  
+  
+app.listen(2000,function(){  
+    console.log("Server is running on port 2000");  
+});  
